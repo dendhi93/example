8ce8c2a58cd34e445970ba822804a016d52b4266
MUF\15997068 <deddy.effendi@muf.co.id>
2018-10-11 11:00:42 +0700

    penambahan profile fragment

--- app/src/main/java/com/example/myapplication/fitur/dashboard/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/com/example/myapplication/fitur/dashboard/viewmodel/DashboardViewModel.kt
@@ -1,9 +1,15 @@
 package com.example.myapplication.fitur.dashboard.viewmodel
 
 import android.content.Context
+import com.example.myapplication.application.BaseApplication
 import com.example.myapplication.fitur.dashboard.contract.DashboardInterface
 
 class DashboardViewModel(val context: Context, val callback: DashboardInterface){
     private val TAG = "DashboardViewModel"
+
+
+    fun welcome(){
+        callback.onShowMessage(BaseApplication.preferences!!.userName)
+    }
 
 }
--- app/src/main/java/com/example/myapplication/utils/Preferences.kt
+++ app/src/main/java/com/example/myapplication/utils/Preferences.kt
@@ -21,10 +21,6 @@ class Preferences(private val context: Context){
         private val KEY_USERNAME = "username"
         private val KEY_PASS = "password"
 
-        private val KEY_NAME = "name"
-        private val KEY_ADDRESS = "address"
-        private val KEY_PHONE = "phone"
-        private val KEY_SHOPNAME = "shopName"
     }
 
     private fun isKeyExist(Key: String): Boolean = sharedPreferences.contains(Key)
@@ -61,21 +57,6 @@ class Preferences(private val context: Context){
             putString(KEY_PASS, "")
         }
 
-        if (!isKeyExist(KEY_NAME)) {
-            putString(KEY_NAME, "")
-        }
-
-        if (!isKeyExist(KEY_ADDRESS)) {
-            putString(KEY_ADDRESS, "")
-        }
-
-        if (!isKeyExist(KEY_PHONE)) {
-            putString(KEY_PHONE, "")
-        }
-
-        if (!isKeyExist(KEY_SHOPNAME)) {
-            putString(KEY_SHOPNAME, "")
-        }
     }
 
     var isLogin: Boolean
@@ -90,21 +71,7 @@ class Preferences(private val context: Context){
         get() = getString(KEY_PASS)
         set(value) = putString(KEY_PASS,value)
 
-    var name : String
-        get() = getString(KEY_NAME)
-        set(value) = putString(KEY_NAME,value)
 
-    var address : String
-        get() = getString(KEY_ADDRESS)
-        set(value) = putString(KEY_ADDRESS,value)
-
-    var phone : String
-        get() = getString(KEY_PHONE)
-        set(value) = putString(KEY_PHONE,value)
-
-    var shopName : String
-        get() = getString(KEY_SHOPNAME)
-        set(value) = putString(KEY_SHOPNAME,value)
 
     private fun resetPref() {
         if (isKeyExist(KEY_IS_LOGIN)) {
@@ -119,21 +86,6 @@ class Preferences(private val context: Context){
             putString(KEY_PASS, "")
         }
 
-        if (isKeyExist(KEY_NAME)) {
-            putString(KEY_NAME, "")
-        }
-
-        if (isKeyExist(KEY_ADDRESS)) {
-            putString(KEY_ADDRESS, "")
-        }
-
-        if (isKeyExist(KEY_PHONE)) {
-            putString(KEY_PHONE, "")
-        }
-
-        if (isKeyExist(KEY_SHOPNAME)) {
-            putString(KEY_SHOPNAME, "")
-        }
     }
 
     fun clearSession() {
